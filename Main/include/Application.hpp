d38dcfb9ee7959f0a191f7f11258131870a077e6
ereti <43418802+ereti@users.noreply.github.com>
2020-12-07 13:31:59 +0000

    Introduce SkinIR to lua as IR, rebrand ir consts to IRState

--- Main/include/Application.hpp
+++ Main/include/Application.hpp
@@ -3,6 +3,7 @@
 #include <Shared/Jobs.hpp>
 #include <Shared/Thread.hpp>
 #include "SkinHttp.hpp"
+#include "SkinIR.hpp"
 
 #define DISCORD_APPLICATION_ID "514489760568573952"
 
@@ -33,7 +34,7 @@ public:
 	void ApplySettings();
 	// Runs the application
 	int32 Run();
-	
+
 	void SetCommandLine(int32 argc, char** argv);
 	void SetCommandLine(const char* cmdLine);
 
@@ -133,6 +134,7 @@ private:
 	Thread m_updateThread;
 	class Beatmap* m_currentMap = nullptr;
 	SkinHttp m_skinHttp;
+	SkinIR m_skinIR;
 
 	float m_lastRenderTime;
 	float m_deltaTime;
